4db0a66f4d8315eb3c2ed0e6d0818891bf74f4e6
Hadar Tayar <hadar6611@gmail.com>
2024-03-10 20:46:37 +0200

    small touches

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import NotesContainer from './Components/NotesContainer';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <h2>Sticky Notes Board</h2>
-      <NotesContainer/>
+      <NotesContainer />
     </div>
   );
 }
